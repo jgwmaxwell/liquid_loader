7dbd61fab523e2f3b42375882f55c20f54ff44e2
John Maxwell <jgwmaxwell@gmail.com>
2011-12-12 15:01:50 +0000

    new

--- lib/liquid_loader.rb
+++ lib/liquid_loader.rb
@@ -2,4 +2,4 @@ require 'rails/generators/base'
 
 require "liquid_loader/version"
 require 'liquid_loader/liquid_model'
-require 'rails/generators/drop_generator'
+require 'rails/generators/liquid/drop_generator'
