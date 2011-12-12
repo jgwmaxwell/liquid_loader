8e3354ea27f45034415eade459cd331ef93ed272
John Maxwell <jgwmaxwell@gmail.com>
2011-12-12 16:44:38 +0000

    test

--- lib/rails/generators/liquid/drop_generator.rb
+++ lib/rails/generators/liquid/drop_generator.rb
@@ -7,8 +7,11 @@ module Liquid
 
       desc "This generator creates liquid drops for your models."
       def create_drop
-        create_file "app/drops/#{name}_drop.rb", "# Add initialization content here"
-        puts "created [app/drops/#{name}_drop.rb]"
+        #create_file "app/drops/#{name}_drop.rb", "# Add initialization content here"
+        puts name
+        puts file_name
+        puts class_name
+
       end
     end
   end
