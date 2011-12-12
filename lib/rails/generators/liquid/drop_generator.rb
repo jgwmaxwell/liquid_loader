1b65c182c990828e5565b0e415e6e97e2954ba93
John Maxwell <jgwmaxwell@gmail.com>
2011-12-12 14:47:13 +0000

    new

---
+++ lib/rails/generators/liquid/drop_generator.rb
@@ -0,0 +1,10 @@
+module Liquid
+  module Generators
+    class DropGenerator < Rails::Generators::Base
+      desc "This generator creates liquid drops for your models."
+      def create_drop
+        create_file "app/drops/_drop.rb", "# Add initialization content here"
+      end
+    end
+  end
+end
