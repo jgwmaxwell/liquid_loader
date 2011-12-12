21859ea42794bc8be892adaba284de5a4cfb034a
John Maxwell <jgwmaxwell@gmail.com>
2011-12-12 22:33:24 +0000

    test

--- lib/liquid_loader/context.rb
+++ lib/liquid_loader/context.rb
@@ -3,10 +3,6 @@ require 'liquid'
 module LiquidLoader
   module Context
 
-    def self.included(base)
-      base.instance_variable_set(:@context, Liquid::Context.new)
-    end
-
     def render_liquid(template, context = @context)
       template.render(context)
     end
@@ -18,19 +14,10 @@ module LiquidLoader
     def add_context(opts = nil)
       if opts.is_a? Hash
         opts.each do |k, v|
-          @context[k.to_s.downcase] = v
-        end
-      end
-    end
-
-    def liq_context(opts = nil)
-      if opts.is_a? Hash
-        opts.each do |k, v|
           liq[k.to_s.downcase] = v
         end
       end
     end
-
 
     private
     def liq
