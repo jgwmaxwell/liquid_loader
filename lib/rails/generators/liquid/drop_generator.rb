3d0213906ed013044d4093629e6a888a6a4697a4
John Maxwell <jgwmaxwell@gmail.com>
2011-12-12 15:37:25 +0000

    hello

--- lib/rails/generators/liquid/drop_generator.rb
+++ lib/rails/generators/liquid/drop_generator.rb
@@ -2,7 +2,7 @@ require 'rails/generators/base'
 
 module Liquid
   module Generators
-    class DropGenerator < Rails::Generators::Base
+    class DropGenerator < Rails::Generators::NamedBase
       desc "This generator creates liquid drops for your models."
       def create_drop
         #create_file "app/drops/_drop.rb", "# Add initialization content here"
