8a57a301ce4299fa7e6ad780ee14e4ad86b62920
John Maxwell <jgwmaxwell@gmail.com>
2011-12-12 17:12:38 +0000

    test

--- lib/rails/generators/liquid/drop_generator.rb
+++ lib/rails/generators/liquid/drop_generator.rb
@@ -2,15 +2,15 @@ require 'rails'
 
 module Liquid
   module Generators
-    class DropGenerator < Rails::Generators::Base
-      source_root File.expand_path('../templates', __FILE__)
+    class DropGenerator < Rails::Generators::NamedBase
+      #source_root File.expand_path('../templates', __FILE__)
       #argument :name, :type => :string
       #class_option :autodiscover, :type => :boolean, :default => true, :description => "Automatically discover attributes and add them to the Drop."
 
 
       desc "This generator creates liquid drops for your models."
 
-      generate_attributes if options.autodiscover?
+      #generate_attributes if options.autodiscover?
 
       def create_drop
         template "drop.rb", "app/drops/#{file_name}_drop.rb"
