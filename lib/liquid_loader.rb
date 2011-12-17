ec3ae583da91e5484a9d94d89dfec93775ff2644
John Maxwell <jgwmaxwell@gmail.com>
2011-12-17 16:12:48 +0000

    added validations

--- lib/liquid_loader.rb
+++ lib/liquid_loader.rb
@@ -4,3 +4,4 @@ require 'active_support/dependencies'
 require "liquid_loader/version"
 require 'liquid_loader/context'
 require 'liquid_loader/liquid_model'
+require 'liquid_loader/liquid_validations'
